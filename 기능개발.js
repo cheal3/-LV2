700d7c2bf380c0c19fed8ade86f7a88d9bb45031
cheal3 <49738628+cheal3@users.noreply.github.com>
2021-01-08 20:37:39 +0900

    기능개발

---
+++ 기능개발.js
@@ -0,0 +1,26 @@
+function solution(progresses, speeds) {
+    var ret = []
+    var count = 0;
+    var pointer = 0; 
+    while(progresses[pointer] < 100){
+        
+    for (var i = 0 ; i < progresses.length ;  i++){
+        progresses[i] += speeds[i]          
+    }  
+        
+    if (progresses[pointer] >= 100){ 
+    for (var i = 0 ; i < progresses.length ; i++){        
+        if (progresses[pointer] >= 100 ){
+            pointer++;
+            count++;   
+        }else{
+            break;
+        }
+
+    }        
+        ret.push(count) 
+        count = 0 ;         
+    }    
+    }    
+    return ret;
+}
