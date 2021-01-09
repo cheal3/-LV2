f6b319b27987c7291cf49cc3349917ef32be7754
cheal3 <49738628+cheal3@users.noreply.github.com>
2021-01-10 01:29:03 +0900

    Update 다리를지나는트럭.js
    성능 보완

--- 다리를지나는트럭.js
+++ 다리를지나는트럭.js
@@ -5,31 +5,29 @@ function solution(bridge_length, weight, truck_weights) {
     var sum = 0
     var popTime = []
     var truck = truck_weights.length
-    var elapsedSec = 1;
+    var elapsedSec = 1;    
+    var temp = truck_weights.shift()
     
-    passing.push(truck_weights.shift());
+    passing.push(temp);
     popTime.push(elapsedSec + bridge_length)
+    sum += temp
     
      while(passed.length != truck){     
-        elapsedSec ++;  // 경과시간 증가   
-         
+        elapsedSec ++;  // 경과시간 증가            
         if(elapsedSec == popTime[0]){ // 다리를 다 건넌 트럭을 이동
-            passed.push(passing.shift())
-            popTime.shift()
+            temp = passing.shift()
+            passed.push(temp)     
+            sum -= temp
+            popTime.shift()            
         }
-         
-        for (var i = 0 ; i < passing.length ; i++){  //지나는 트럭의 무게
-            sum += passing[i]           
-        }  
-         
+
         if (truck_weights[0]!= undefined && truck_weights[0] + sum <= weight) {       // 트럭이 더 들어갈 수 있으면 넣기
-            passing.push(truck_weights.shift());
-            popTime.push(elapsedSec + bridge_length)
+            temp = truck_weights.shift()
+            passing.push(temp);
+            sum += temp
+            popTime.push(elapsedSec + bridge_length)            
         }
-        
-        
-        sum = 0 ;
-}
-    
+         
+}    
     return elapsedSec;
 }
