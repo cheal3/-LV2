b5142200fedaf1f84ee18fd5e9cb4cb41fb6c76f
cheal3 <49738628+cheal3@users.noreply.github.com>
2021-01-10 01:56:06 +0900

    성능 보완

--- 다리를지나는트럭.js
+++ 다리를지나는트럭.js
@@ -1,33 +1,31 @@
 function solution(bridge_length, weight, truck_weights) {
     var answer = 0;
-    var passed = []
-    var passing = []
-    var sum = 0
-    var popTime = []
-    var truck = truck_weights.length
-    var elapsedSec = 1;    
-    var temp = truck_weights.shift()
+    var passed = 0;
+    var passing = [];
+    var popTime = [];
+    var truck = truck_weights.length;
+    var elapsedSec = 1;
+    var temp = 0;
+    var sum = truck_weights[passed + passing.length];
+    passing.push(truck_weights[passed + passing.length]);
+    popTime.push(elapsedSec + bridge_length);   
     
-    passing.push(temp);
-    popTime.push(elapsedSec + bridge_length)
-    sum += temp
-    
-     while(passed.length != truck){     
-        elapsedSec ++;  // 경과시간 증가            
-        if(elapsedSec == popTime[0]){ // 다리를 다 건넌 트럭을 이동
-            temp = passing.shift()
-            passed.push(temp)     
-            sum -= temp
-            popTime.shift()            
-        }
-
-        if (truck_weights[0]!= undefined && truck_weights[0] + sum <= weight) {       // 트럭이 더 들어갈 수 있으면 넣기
-            temp = truck_weights.shift()
-            passing.push(temp);
-            sum += temp
-            popTime.push(elapsedSec + bridge_length)            
-        }
+   while(passed != truck) {     
+      elapsedSec ++;  // 경과시간 증가            
+      if(elapsedSec == popTime[0]){ // 다리를 다 건넌 트럭을 이동
+         passed++;         
+         temp = passing.shift();
+         sum -= temp;   
+         popTime.shift();  
+      }            
+      if ( truck_weights[passed + passing.length] != undefined && truck_weights[passed + passing.length] + sum <= weight) {       // 트럭이 더 들어갈 수 있으면 넣      
+         sum += truck_weights[passed + passing.length]
+         passing.push(truck_weights[passed + passing.length]);    
          
-}    
+        
+         popTime.push(elapsedSec + bridge_length);
+      }        
+    
+   }
     return elapsedSec;
 }
